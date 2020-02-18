7e89cbf3da00b46451c49f263d07960b4277d534
Leo Kim <sotysk@gmail.com>
2020-02-18 22:10:41 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,14 +3,18 @@ if speed < 40
   TRUE
 elsif speed > 60
 TRUE
-else
+elsif speed == 30
+TRUE
+elsif speed == 95
+TRUE
+else speed == 50
+FALSE
 end
 
 
 
 def not_safe?(speed)
-if
-else
+
 end
 	
 
