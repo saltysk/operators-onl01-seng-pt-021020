83f4d0975253557171215b9e1b3ba49c1513e9c3
Leo Kim <sotysk@gmail.com>
2020-02-18 22:23:50 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,21 @@
+require 'pry'
 def unsafe?(speed)
-
+  #binding.pry
+if speed > 60
+true
+elsif speed < 40
+true
+elsif speed >= 40 && speed <= 60
+  false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+# (conditional) ? 'true/truthy' : 'false/falsey'
+
+(speed >= 40 && speed <= 60) ? false : true
 end
 	
 
